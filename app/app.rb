f56315800cc0ecf40c413a6674b6021f61efadd2
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-06 16:50:12 +0100

    Step #16 completed, filtering added

--- app/app.rb
+++ app/app.rb
@@ -23,6 +23,12 @@ class Bookmark < Sinatra::Base
     redirect '/links'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = (tag ? tag.links : [])
+    erb(:'links/index')
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,6 +8,6 @@ class Tag
   property :id,   Serial
   property :name, String
 
-  #has n, :links
+  has n, :links, through: Resource
 
 end
