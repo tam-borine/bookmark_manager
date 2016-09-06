8647461362a7b0da7b2792943d91f483fa64df1c
Tam Borine <tamborine@Tams-MacBook-Air.local>
2016-09-06 10:52:58 +0100

    added links list view

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
+require_relative './models/link'
 
 class Bookmark < Sinatra::Base
   get '/' do
+    @links = Link.all
     erb :index
   end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
