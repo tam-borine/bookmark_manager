31e54145865473b10cc8f6dabc9ae02d508a845a
ercekal <ercekalabalikoglu@gmail.com>
2016-09-07 15:06:33 +0100

    search option of tag is added for ch 16. Understood up to a certain point...

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,11 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class Tag
+
   include DataMapper::Resource
+
+  has n, :links, through: Resource
+
   property :id, Serial
   property :name, String
 end
